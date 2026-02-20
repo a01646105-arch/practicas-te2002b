74bbaed47abb985c628dc66af049c960f0d409cf
a01646105-arch <a01646105@tec.mx>
2026-02-19 20:19:31 -0600

    Update BCD_4displays.v

--- practica_2_BCD_4displays/BCD_4displays.v
+++ practica_2_BCD_4displays/BCD_4displays.v
@@ -1,6 +1,6 @@
 module BCD_4displays #(parameter N_in = 10, N_out = 7)(
     input [N_in-1:0] bcd_in,
-    output [N_out-1:0] D_un, D_de, D_ce, D_mi
+    output [0:N_out-1] D_un, D_de, D_ce, D_mi // Por convención, las señales del display de 7 segmentos empieza con el segmento 0
 );
 
 wire [3:0] unidades, decenas, centenas, millares;
