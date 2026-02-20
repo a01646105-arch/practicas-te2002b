881fa1cf3c495b4e433edfec8e0f54b8a4503d6d
a01646105-arch <a01646105@tec.mx>
2026-02-19 20:21:08 -0600

    Update BCD_module.v

--- practica_2_BCD_4displays/BCD_module.v
+++ practica_2_BCD_4displays/BCD_module.v
@@ -1,22 +1,22 @@
 module BCD_module(
     input [3:0] BCD_in,
-    output reg [6:0] BCD_out
+    output reg [0:6] BCD_out // Por convención, las señales del display de 7 segmentos empieza con el segmento 0
 );
 
 always @(*)
     begin
         case(BCD_in)
-            0: BCD_out = 7'b1111_110;
-            1: BCD_out = 7'b0110_000;
-            2: BCD_out = 7'b1101_101;
-            3: BCD_out = 7'b1111_001;
-            4: BCD_out = 7'b0110_011;
-            5: BCD_out = 7'b1011_011;
-            6: BCD_out = 7'b1011_111;
-            7: BCD_out = 7'b1110_000;
-            8: BCD_out = 7'b1111_111;
-            9: BCD_out = 7'b1110_011;
-            default: BCD_out = 7'b0000_000;
+            0: BCD_out = 7'b0000_001; // Las señales del display de 7 segmentos están invertidas
+            1: BCD_out = 7'b1001_111;
+            2: BCD_out = 7'b0010_010;
+            3: BCD_out = 7'b0000_110;
+            4: BCD_out = 7'b1001_100;
+            5: BCD_out = 7'b0100_100;
+            6: BCD_out = 7'b0100_000;
+            7: BCD_out = 7'b0001_111;
+            8: BCD_out = 7'b0000_000;
+            9: BCD_out = 7'b0001_100;
+            default: BCD_out = 7'b1111_111;
         endcase
     end
 
